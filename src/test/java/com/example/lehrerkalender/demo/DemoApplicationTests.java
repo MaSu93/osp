2f5510664cfa97f3847a102a27c5958c26690e7d
Alexander Goujov <alexander.goujov@compana.net>
2019-11-13 13:11:45 +0100

    test code pushed

---
+++ src/test/java/com/example/lehrerkalender/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lehrerkalender.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
