9d0aa1c238f44f47b3fdc96bebbbf98547bb212b
Alexander Goujov <alexander.goujov@compana.net>
2019-11-14 13:23:26 +0100

    Relationen zwischen den Objekten hinzugefügt

--- src/main/java/com/example/lehrerkalender/demo/model/Kurs.java
+++ src/main/java/com/example/lehrerkalender/demo/model/Kurs.java
@@ -13,7 +13,13 @@ public class Kurs {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "idLehrer")
     private Lehrer lehrer;
+    @ManyToOne
+    @JoinColumn(name = "idKlasse")
     private Klasse klasse;
+    @ManyToOne
+    @JoinColumn(name = "idFach")
     private Fach fach;
 }
