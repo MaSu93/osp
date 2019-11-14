27a3d17a31628a510e9572af581f237a3306e23f
jonas.kuepper <rohrkrepierer@protonmail.com>
2019-11-14 11:20:47 +0100

    GeneratedValue hinzugefügt

--- src/main/java/com/example/lehrerkalender/demo/model/Fach.java
+++ src/main/java/com/example/lehrerkalender/demo/model/Fach.java
@@ -4,9 +4,7 @@ package com.example.lehrerkalender.demo.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "fach")
@@ -14,6 +12,7 @@ import javax.persistence.Table;
 @Setter
 public class Fach {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 }
